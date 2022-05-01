4cab2cf5403626072caef610e3fac6a7c68d7d3b
Numeritxs <ivanamca01@gmail.com>
2022-05-01 14:06:08 +0200

    added .sql file

---
+++ stucomivan.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-05-2022 a las 13:41:41
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `stucomivan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gestores`
+--
+
+CREATE TABLE `gestores` (
+  `user` varchar(20) NOT NULL,
+  `password` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `gestores`
+--
+
+INSERT INTO `gestores` (`user`, `password`) VALUES
+('test', '$2y$10$4mCa0XzHN8/2gAqytZ3fw.zFQfP6ZD3qtb.DET6hzubrCVqbjdF0C');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos`
+--
+
+CREATE TABLE `modulos` (
+  `nombre` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`nombre`) VALUES
+('web'),
+('sistemas'),
+('java'),
+('redes'),
+('bases de datos'),
+('english');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos_profes`
+--
+
+CREATE TABLE `modulos_profes` (
+  `dni` varchar(200) NOT NULL,
+  `nombre` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `modulos_profes`
+--
+
+INSERT INTO `modulos_profes` (`dni`, `nombre`) VALUES
+('49327478E', 'sistemas'),
+('49327478E', 'web'),
+('49327478E', 'english'),
+('49327478E', 'bases de datos'),
+('tfghdfghd', 'web'),
+('tfghdfghd', 'java'),
+('tfghdfghd', 'english'),
+('tfghdgfdsfd', 'sistemas'),
+('tfghdgfdsfd', 'java'),
+('profanar la tumba al ritmo de la rumba', 'redes'),
+('select * from modulos', 'java');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `nombre` varchar(20) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `edad` int(2) NOT NULL,
+  `dni` varchar(9) NOT NULL,
+  `ciclos` text NOT NULL,
+  `colegio` varchar(50) NOT NULL,
+  `fecha_alta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `profesores`
+--
+
+INSERT INTO `profesores` (`nombre`, `apellidos`, `edad`, `dni`, `ciclos`, `colegio`, `fecha_alta`) VALUES
+('crisitanhfdgfghjfdgs', 'catalan', 42, '37564265K', 'srgsdrf', 'Stucom', '2022-05-01'),
+('grfdhbfg', 'hgfdhfghgf', 22, '46578029Q', 'hfdghgdf', 'fgdhdfg', '2022-05-01'),
+('ivansito', 'amelasion', 22, '49327478E', 'daw1', '', '2022-04-13'),
+('gdrfsgdfg', 'sfdhfdghgdfh', 22, 'tfghdfghd', 'hfgdhfgdhfg', 'hfgdhhfgh', '2022-04-30');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `gestores`
+--
+ALTER TABLE `gestores`
+  ADD PRIMARY KEY (`user`);
+
+--
+-- Indices de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`dni`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
